2eb39f2f72f90aead4716757e46e9be61a836d1c
fellypr <fellype29kennedemil@gmail.com>
2025-06-10 15:05:17 -0300

    Finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,35 @@
-import { useState , useRef} from 'react'
+import { useState ,useEffect} from 'react'
 import './App.css'
 import axios from 'axios';
 import Weatherinfor from './components/weatherinfor/info';
 
 function App() {
   const [weather,setWeather] = useState();
-  const inputref = useRef(); //useref tem como função mostra a referencia , e a variavel "inputref" chama ela.
+  const [SearchCity, setSearchCity] = useState("");
   
   async function searchCity(){
-    const city = inputref.current.value;
     const key = "e02d225f58791b76945b2c0c4dee1cfb";
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${SearchCity}&appid=${key}&lang=pt_br&units=metric`;
     const apidata = await axios.get(url);
     setWeather(apidata.data)
+    console.log(apidata.data);
   }
+  useEffect(() => {
+    if(SearchCity.length > 0){
+      searchCity();
+    }else{
+      setSearchCity("");
+      setWeather("");
+    }
+  },[SearchCity])
 
   return (
     <>
       <div className='screenfirt'>
           <h1>Previsão do Tempo</h1>
-          <input type="text" placeholder='Digite o nome da cidade' ref={inputref}/>
-          <button onClick={searchCity}>Buscar</button>
+          <input type="text" placeholder='Digite o nome da cidade' onChange={(e) => setSearchCity(e.target.value)}/>
+          {weather && <Weatherinfor weather = {weather}/>}
       </div>
-      {weather && <Weatherinfor weather = {weather}/>}
-      
     </>
   )
 }
--- src/components/weatherinfor/info.jsx
+++ src/components/weatherinfor/info.jsx
@@ -9,17 +9,13 @@ function Weatherinfor ({weather}){
             <h2>{weather.name}</h2>
 
             <div className='info'>
-                <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} width={80}/>
+                <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} width={100}/>
                 <p>{Math.round(weather.main.temp)}ºC</p>
-            </div>
                 <p className='description'>{weather.weather[0].description}</p>
-            <div className='infoprofun'>
                 <p>Sensação termica:{Math.round(weather.main.feels_like)}ºC</p>
                 <p>Umidade:{weather.main.humidity}%</p>
                 <p>pressão:{weather.main.pressure}</p>
             </div>
-            
-
         </div>
     );
 };
